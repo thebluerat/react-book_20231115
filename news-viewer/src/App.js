bdffd8932be2cef131e93f9554ef63a0d322d3ce
soomin <riato101@naver.com>
2023-12-02 02:27:22 +0900

    20231202 p369. 뉴스 뷰어 UI 만들기

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,28 +1,8 @@
 import React, {useState} from 'react';
-import axios from 'axios';
+import NewsList from './components/NewsList';
 
 const App =()=> {
-  const [data, setData] = useState(null);
-  const onClick = async ()=> {
-    try {
-      const response = await axios.get(
-        'https://newsapi.org/v2/top-headlines?country=kr&category=science&apiKey=b77c596bbc714b02af63ba6efa4a9989',
-        );
-        setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-  return (
-    <>
-      <div>
-        <div>
-          <button onClick={onClick}>불러오기</button>
-        </div>
-        {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true}/>}
-      </div>
-    </>
-  )
+  return <NewsList/>
 }
 
 export default App;
